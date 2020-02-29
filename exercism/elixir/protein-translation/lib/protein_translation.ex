4f1f7b51a9a52dbf9f4d5ec194f4429a3c1e168a
An Nyeong <me@annyeong.me>
2020-02-29 17:50:15 +0900

    Exercise protein-translation

---
+++ exercism/elixir/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,76 @@
+defmodule ProteinTranslation do
+  @amnio_acids %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    proteins = rna
+               |> String.to_charlist
+               |> Enum.chunk_every(3)
+               |> Enum.map(&to_string/1)
+               |> Enum.reduce_while([], fn x, acc ->
+                  case of_codon(x) do
+                    {:ok, "STOP"} -> {:halt, acc}
+                    {:ok, protein} -> {:cont, [protein | acc]}
+                    _ -> {:halt, []}
+                  end
+                end)
+
+    unless Enum.empty?(proteins) do
+      {:ok, proteins |> Enum.reverse}
+    else
+      {:error, "invalid RNA"}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    protein = Map.get(@amnio_acids, codon)
+    if protein do
+      {:ok, protein}
+    else
+      {:error, "invalid codon"}
+    end
+  end
+end
