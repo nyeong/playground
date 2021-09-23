de7592e3ccd739385bdfcdef67738ee6f8e938d3
An Nyeong <me@annyeong.me>
2021-09-23 12:53:40 +0900

    Remove unnecessary files

--- qna/jangsangmin/test1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  char * buffer = "wishfree\n%x\n";
-  printf(buffer);
-}
--- qna/jangsangmin/wrong.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  char *buffer = "wishfree\n";
-  printf(buffer);
-
-  return 0;
-}
